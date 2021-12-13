a9059c5db2e98ce8cba1580b756d09e18ebcbf09
Jakub Bączek <baczek.jakub@gmail.com>
2021-12-13 22:54:19 +0100

    game desc

--- 5-seconds/src/components/Header.jsx
+++ 5-seconds/src/components/Header.jsx
@@ -7,19 +7,19 @@ export default function Header(props){
         <div>
             <Navbar bg="light" expand="lg" style={{height: '70px'}}>
                 <Container>
-                    <Navbar.Brand style={{marginRight: '40px'}}><h1>5 seconds</h1></Navbar.Brand>
+                    <Navbar.Brand style={{marginRight: '40px'}}><h1>5 sekund</h1></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link style ={{fontSize: 20, marginTop: '15px'}}>Home</Nav.Link>
+                            <Nav.Link style ={{fontSize: 20, marginTop: '15px'}} onClick={()=>props.setGameState('mainPage')}>Home</Nav.Link>
                             {(props.gameState=="mainPage")?
-                            <NavDropdown title="Start game" id="basic-nav-dropdown" style = {{fontSize: 40, marginLeft:'235px'}}>
-                                <NavDropdown.Item onClick={() => props.setGameState('2Players')}>2 Players</NavDropdown.Item>
-                                <NavDropdown.Item onClick={() => props.setGameState('3Players')}>3 Players</NavDropdown.Item>
-                                <NavDropdown.Item onClick={() => props.setGameState('4Players')}>4 Players</NavDropdown.Item>
-                                <NavDropdown.Item onClick={() => props.setGameState('5Players')}>5 Players</NavDropdown.Item>
+                            <NavDropdown title="Rozpocznij gre" id="basic-nav-dropdown" style = {{fontSize: 40, marginLeft:'220px'}}>
+                                <NavDropdown.Item onClick={() => props.setGameState('2Players')}>2 Graczy</NavDropdown.Item>
+                                <NavDropdown.Item onClick={() => props.setGameState('3Players')}>3 Graczy</NavDropdown.Item>
+                                <NavDropdown.Item onClick={() => props.setGameState('4Players')}>4 Graczy</NavDropdown.Item>
+                                <NavDropdown.Item onClick={() => props.setGameState('5Players')}>5 Graczy</NavDropdown.Item>
                             </NavDropdown>:
-                            <Nav.Link style={{fontSize: 40, marginLeft:'270px'}} onClick={()=>props.setGameState('mainPage')}><h1>End Game</h1></Nav.Link>
+                            <Nav.Link style={{fontSize: 40, marginLeft:'270px'}} onClick={()=>props.setGameState('mainPage')}><h1>Zakończ gre</h1></Nav.Link>
                             }
                         </Nav>
                     </Navbar.Collapse>
--- 5-seconds/src/components/MainPage.jsx
+++ 5-seconds/src/components/MainPage.jsx
@@ -1,11 +1,24 @@
 import React, {useState} from 'react'
-
+import {Card} from 'react-bootstrap/'
 
 export default function MainPage(props){
     if(props.gameState=='mainPage')
     {
     return(
-            <p class = 'fw-bolder'>xdd</p>
+        <div>
+            <div style={{width:'100%', height:'160px', float:'left'}}></div>
+            <Card style={{ width: '18rem' , margin: '0 auto'}}>
+                <Card.Body>
+                <Card.Title>Zasady gry</Card.Title>
+                <Card.Text>
+                    W zależności od wybranej opcji w grze bierze 
+                    udział 2, 3, 4 lub 5 graczy. Na ekranie będą pokazywały się kartyz pytaniami, na które trzeba udzielic 3 
+                    odpowiedzi w ciągu tytułowych 5 sekund, każdy któremu się to uda powinien otrzymać punkt. Dla pomocy z boku strony będzie 
+                    pokazany punktacja graczy a u góry licznik czasu pozostałego na odpowiedź. 
+                </Card.Text>
+                </Card.Body>
+            </Card>
+          </div>
         );
     }
     else
