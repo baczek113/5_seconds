7cf95896cf416426e3f228d174de78175318568e
Jakub Bączek <baczek.jakub@gmail.com>
2021-12-13 21:48:38 +0100

    dziala main page

--- 5-seconds/src/components/MainPage.jsx
+++ 5-seconds/src/components/MainPage.jsx
@@ -1,8 +1,17 @@
 import React, {useState} from 'react'
 
 
-export default function MainPage(){
-        return(
+export default function MainPage(props){
+    if(props.gameState=='mainPage')
+    {
+    return(
             <p class = 'fw-bolder'>xdd</p>
         );
+    }
+    else
+    {
+        return(
+            <p class = 'fw-bolder'>tera ingame</p>
+        );
+    }
 }
