88818c0e9cef02f2d07a2bd2c1651f4aac61c814
poggerfish <baczek.jakub@gmail.com>
2021-12-16 23:04:38 +0100

    nie dziala :)

--- 5-seconds/src/components/ingamecomponents/MainPanel.jsx
+++ 5-seconds/src/components/ingamecomponents/MainPanel.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Container} from 'react-bootstrap/'
 
 export default function MainPanel({players, setPlayers, gameState, setGameState, setInGame}){
+    let [questions, setQuestions] = useState(['dupa', 'cyce', 'wadowice'])
+    let [code, setCode] = useState([])
+    
     return(
         <div>
-            {gameState}
+            xd
         </div>
     )
 }
--- 5-seconds/src/components/ingamecomponents/Scoreboard.jsx
+++ 5-seconds/src/components/ingamecomponents/Scoreboard.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import {Row, Col, Container} from 'react-bootstrap/'
+import {React, useState} from 'react'
+import {Row, Col, Container, Button} from 'react-bootstrap/'
 
 export default function Scoreboard({players, setPlayers, gameState}){
     let temp
-    let full = [<Row><Col>Gracz:</Col><Col>Punkty:</Col></Row>]
+    let [full, setFull] = useState([])
     for(let i = 0; parseInt(gameState.substring(0, 1))>i; i++)
     {
         temp=<Row style = {{marginTop: '5px'}}>
@@ -11,10 +11,13 @@ export default function Scoreboard({players, setPlayers, gameState}){
             <Col sm>{players[i][0]}</Col>
         </Row>
         full.push(temp)
-        console.log(full)
     }
     return(
         <div style = {{float: 'right', width: '22.5%', marginRight: '40px', marginTop: '40px', border: '1px solid #808080', borderRadius: '5px', padding: '10px'}}>
+            <Row style = {{marginTop: '5px'}}>
+                <Col sm>Gracze</Col>
+                <Col sm>Punkty</Col>
+            </Row>
             {full.map(item=>item)}
         </div>
     )
