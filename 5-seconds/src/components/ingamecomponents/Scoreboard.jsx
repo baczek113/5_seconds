3bc5e4c3f140435da6d1e715e856dff2a51b2e08
poggerfish <baczek.jakub@gmail.com>
2021-12-17 22:59:39 +0100

    xddd

--- 5-seconds/src/components/ingamecomponents/Scoreboard.jsx
+++ 5-seconds/src/components/ingamecomponents/Scoreboard.jsx
@@ -3,22 +3,105 @@ import {Row, Col, Container, Button} from 'react-bootstrap/'
 
 export default function Scoreboard({players, setPlayers, gameState}){
     let temp
-    let [full, setFull] = useState([])
-    for(let i = 0; parseInt(gameState.substring(0, 1))>i; i++)
+    const addPoints= (number) =>
     {
-        temp=<Row style = {{marginTop: '5px'}}>
-            <Col sm>{players[i][1]}</Col>
-            <Col sm>{players[i][0]}</Col>
-        </Row>
-        full.push(temp)
+        temp = players
+        temp[number][0]++
+        setPlayers(temp)
     }
+    switch(parseInt(gameState.substring(0, 1))){
+    case 2:
     return(
         <div style = {{float: 'right', width: '22.5%', marginRight: '40px', marginTop: '40px', border: '1px solid #808080', borderRadius: '5px', padding: '10px'}}>
-            <Row style = {{marginTop: '5px'}}>
-                <Col sm>Gracze</Col>
-                <Col sm>Punkty</Col>
+            <Row>
+                <Col>Gracze</Col>
+                <Col>Punkty</Col>
             </Row>
-            {full.map(item=>item)}
+            <Row>
+                <Col>{players[0][1]}</Col>
+                <Col>{players[0][0]}</Col>
+            </Row>
+            <Row>
+                <Col>{players[1][1]}</Col>
+                <Col>{players[1][0]}</Col>
+            </Row>
         </div>
     )
+    case 3:
+        return(
+            <div style = {{float: 'right', width: '22.5%', marginRight: '40px', marginTop: '40px', border: '1px solid #808080', borderRadius: '5px', padding: '10px'}}>
+                <Row>
+                    <Col>Gracze</Col>
+                    <Col>Punkty</Col>
+                </Row>
+                <Row>
+                    <Col>{players[0][1]}</Col>
+                    <Col>{players[0][0]}</Col>
+                </Row>
+                <Row>
+                    <Col>{players[1][1]}</Col>
+                    <Col>{players[1][0]}</Col>
+                </Row>
+                <Row>
+                    <Col>{players[2][1]}</Col>
+                    <Col>{players[2][0]}</Col>
+                </Row>
+            </div>
+        )
+        case 4:
+        return(
+            <div style = {{float: 'right', width: '22.5%', marginRight: '40px', marginTop: '40px', border: '1px solid #808080', borderRadius: '5px', padding: '10px'}}>
+                <Row>
+                    <Col>Gracze</Col>
+                    <Col>Punkty</Col>
+                </Row>
+                <Row>
+                    <Col>{players[0][1]}</Col>
+                    <Col>{players[0][0]}</Col>
+                </Row>
+                <Row>
+                    <Col>{players[1][1]}</Col>
+                    <Col>{players[1][0]}</Col>
+                </Row>
+                <Row>
+                    <Col>{players[2][1]}</Col>
+                    <Col>{players[2][0]}</Col>
+                </Row>
+                <Row>
+                    <Col>{players[3][1]}</Col>
+                    <Col>{players[3][0]}</Col>
+                </Row>
+            </div>
+        )
+        case 5:
+        return(
+            <div style = {{float: 'right', width: '22.5%', marginRight: '40px', marginTop: '40px', border: '1px solid #808080', borderRadius: '5px', padding: '10px'}}>
+                <Row>
+                    <Col>Gracze</Col>
+                    <Col>Punkty</Col>
+                </Row>
+                <Row>
+                    <Col>{players[0][1]}</Col>
+                    <Col>{players[0][0]}</Col>
+                </Row>
+                <Row>
+                    <Col>{players[1][1]}</Col>
+                    <Col>{players[1][0]}</Col>
+                </Row>
+                <Row>
+                    <Col>{players[2][1]}</Col>
+                    <Col>{players[2][0]}</Col>
+                </Row>
+                <Row>
+                    <Col>{players[3][1]}</Col>
+                    <Col>{players[3][0]}</Col>
+                </Row>
+                <Row>
+                    <Col>{players[4][1]}</Col>
+                    <Col>{players[4][0]}</Col>
+                </Row>
+                <button onClick={addPoints(1)}>xd</button>
+            </div>
+        )
+    }
 }
