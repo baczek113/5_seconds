9f42a99958e41c35eb23438000e3f9f1f64a77dd
poggerfish <baczek.jakub@gmail.com>
2021-12-22 01:49:29 +0100

    its soooo close to working

--- 5-seconds/src/components/ingamecomponents/MainPanel.jsx
+++ 5-seconds/src/components/ingamecomponents/MainPanel.jsx
@@ -3,10 +3,11 @@ import card from './card.png'
 import {Button} from 'react-bootstrap/'
 
 export default function MainPanel({players, setPlayers, gameState, setGameState, setInGame}){
-    let [questions, setQuestions] = useState(['1', '2', '3', '4', '5'])
+    let [questions, setQuestions] = useState(['1', '2'])
     let [whichPlayer, setWhichPlayer] = useState('')
     let [question, setQuestion] = useState('')
     let [timer, setTimer] = useState('')
+    let [gameOver, setGameOver] = useState(false)
     let [start, setStart] = useState(<div className="mb-2" style = {{position: 'absolute', top: '420px', left: '562px'}} onClick = {()=>{setWhichPlayer(0); setStart('')}}>
     <Button variant="secondary" size="lg">
       Zacznij gre
@@ -44,6 +45,10 @@ export default function MainPanel({players, setPlayers, gameState, setGameState,
             }
             else{
                 setWhichPlayer(0)
+            }
+            if(temp.length===0)
+            {
+                setGameOver(true)
             }}}>
             Odpowiedział na czas
           </Button>
@@ -52,13 +57,36 @@ export default function MainPanel({players, setPlayers, gameState, setGameState,
                 setWhichPlayer(whichPlayer+1)
             }
             else{
-                setWhichPlayer(0)}}}>
+                setWhichPlayer(0)}
+            if(temp.length===0)
+            {
+                setGameOver(true)
+            }}}>
             Nie odpowiedział na czas
             </Button></div>)
         }, 5000)
     }
     }, [whichPlayer])
-
+    const getHighestScore = () => 
+    {
+        let arr = []
+        let max, num
+        
+        players.points.forEach(element =>{
+            arr.push(element)
+        })
+        max = Math.max(arr)
+        for(let i = 0; i<arr.length; i++)
+        {
+            if(arr[i]===max)
+            {
+                num = i
+            }
+        }
+        return(num)
+    }
+    if(gameOver===false)
+    {
     return(
         <div style = {{width: '50%', height: '100%', float: 'left'}}>
             <img src = {card} style = {{height: '634.5px', width: '430.5px', marginLeft: '400px', marginTop: '100px'}}></img>
@@ -67,4 +95,14 @@ export default function MainPanel({players, setPlayers, gameState, setGameState,
             {start}
         </div>
     )
+    }
+    else
+    {
+        return(
+            <div style = {{width: '50%', height: '100%', float: 'left'}}>
+                <p style={{marginLeft:'50%', marginTop: '15%', fontSize: '40px', fontWeight: 'bold', color: 'grey'}}>Wygral:</p><br></br>
+                <Button variant="secondary" size="lg" style = {{color: 'lightgrey', marginLeft: '60%'}} onClick = {()=>{setInGame(false); setGameState('mainPage'); setPlayers([]); setGameOver(false)}}>Wróć do strony głównej</Button>
+            </div>
+        )
+    }
 }
